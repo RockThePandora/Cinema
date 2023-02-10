46e84560a45e11bfee9e9c1441f3a3aa8352b539
RockThePandora <92234814+RockThePandora@users.noreply.github.com>
2023-02-10 15:34:34 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,6 @@
 #include "custom-types/shared/coroutine.hpp"
 #include "assets.hpp"
 #include "PythonInternal.hpp"
-#include "FileUtils.hpp"
-#include "StringUtils.hpp"
-#include "Python.hpp"
 #include "CustomLogger.hpp"
 #include "ModInfo.hpp"
 
