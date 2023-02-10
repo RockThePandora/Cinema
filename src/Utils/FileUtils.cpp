9a1817318e9716fc399d0715a9b07e10a0077994
RockThePandora <92234814+RockThePandora@users.noreply.github.com>
2023-02-10 15:42:33 -0800

    Update FileUtils.cpp

--- src/Utils/FileUtils.cpp
+++ src/Utils/FileUtils.cpp
@@ -9,7 +9,7 @@
 namespace FileUtils {
 
     const std::string& getPythonPath() {
-        static std::string pythonHome = string_format("%spython", Modloader::getDestinationPath().c_str());
+        //static std::string pythonHome = string_format("%spython", Modloader::getDestinationPath().c_str());
         return pythonHome;
     }
 
