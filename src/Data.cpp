dc779ea5a1d77c2fe7b8ec68de3cb54b5521ada9
RockThePandora <92234814+RockThePandora@users.noreply.github.com>
2023-02-11 17:40:38 -0800

    Update Data.cpp

--- src/Data.cpp
+++ src/Data.cpp
@@ -5,7 +5,6 @@
 #include "song-details/shared/Data/RankedStatus.hpp"
 #include <string>
 #include <vector>
-#include <cmath>
 
 namespace SongDetailsCache {
 
@@ -23,53 +22,13 @@ namespace SongDetailsCache {
 
             /// @return Cover url on beatsaver
             std::string coverURL() noexcept;
-
-            /// @brief check if this song is the same as the other, by checking the pointer. Since we do not allow copy construction we can just compare pointers (test pls)
-            /// @param other the Song to check against
-            /// @return equivalency
-            //bool operator ==(Song& other) noexcept {
-                //return this == &other;
-            }
-
-            /// @brief checks if this song is the same as none
-            //inline operator bool() noexcept {
-                //return this != &SongDifficulty::none;
-
-            /// @brief Helper function to get a difficulty from this song
-            /// @param outDiff a reference to the output pointer
-            /// @param diff the difficulty to search for
-            /// @param characteristic the characteristic to search for
-            /// @return whether it was found
-            //bool GetDifficulty(SongDifficulty*& outDiff, MapDifficulty diff, MapCharacteristic characteristic = MapCharacteristic::Standard) noexcept;
-            /// @brief Helper function to get a difficulty from this song
-            /// @param outDiff a reference to the output pointer
-            /// @param diff the difficulty to search for
-            /// @param characteristic the characteristic to search for as a string
-            /// @return whether it was found
-            //bool GetDifficulty(SongDifficulty*& outDiff, MapDifficulty diff, std::string_view characteristic) noexcept;
-            /// @brief Helper function to get a difficulty from this song
-            /// @param diff the difficulty to search for
-            /// @param characteristic the characteristic to search for
-            /// @return The found SongDifficulty, otherwise SongDifficulty::none
-            //SongDifficulty& GetDifficulty(MapDifficulty diff, MapCharacteristic characteristic = MapCharacteristic::Standard) noexcept;
-            /// @brief Helper function to get a difficulty from this song
-            /// @param diff the difficulty to search for
-            /// @param characteristic the characteristic to search for as a string
-            /// @return The found SongDifficulty, otherwise SongDifficulty::none
-            //SongDifficulty& GetDifficulty(MapDifficulty diff, std::string_view characteristic) noexcept;
+}
 
             // allow iterating all difficulties of this song in a regular foreach loop
             using difficulty_const_iterator = std::vector<SongDifficulty>::const_iterator;
             difficulty_const_iterator begin() noexcept;
             difficulty_const_iterator end() noexcept;
-            /// @brief none song, exists to provide an "invalid" song
-            //static Song SongDifficulty::none;
-            /// @brief default move ctor
-            //Song(Song&&) = default;
-            /// @brief delete copy constructor
-            //Song(Song&) = delete;
-            /// @brief this needs to be public for specific reasons, but it's not advised to make your own SongDetail::Songs!
-            Song(std::size_t index, std::size_t diffOffset, uint8_t diffCount, Structs::SongProto* proto) noexcept;
+
         private:
             friend class SongDetailsContainer;
             friend class SongDetails;
